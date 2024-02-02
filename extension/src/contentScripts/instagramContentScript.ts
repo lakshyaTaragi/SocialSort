b9c783258a89941da249fa073e02563102578511
Lakshya Taragi <lakshya.taragi01@gmail.com>
2024-02-02 22:20:00 +0530

    Configure vite to work with manifest

---
+++ extension/src/contentScripts/instagramContentScript.ts
@@ -0,0 +1,5 @@
+console.log('hi from instagramContentScript.js')
+let arr: number[] = [1, 2, 3];
+arr.forEach((num: number) => {
+    console.log(num, "!!!")
+})
--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        'instagramContentScript': './src/contentScripts/instagramContentScript.ts',
+        'index': 'index.html',
+      },
+      output: {
+        entryFileNames: ({ name }) => `assets/${name}.js`,
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      }
+    }
+  }
 })
